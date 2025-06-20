6028c9cc16ffd923302a357165a74641d4daf04a
Facundo Gabriel Wehrli <wehrlifacundo9@gmail.com>
2025-06-20 09:50:41 -0300

    initial commit

---
+++ src/main/java/com/mycompany/doggroomingsalon/Doggroomingsalon.java
@@ -0,0 +1,10 @@
+package com.mycompany.doggroomingsalon;
+
+public class Doggroomingsalon {
+
+    public static void main(String[] args) {
+        //TODO add project to github
+        //start the project
+        //Pawfection Grooming 
+    }
+}   
