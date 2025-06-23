4daa9a4752be9cc03e43bf2a746d2a481f093981
Facundo Gabriel Wehrli <wehrlifacundo9@gmail.com>
2025-06-23 15:00:27 -0300

    added service files

---
+++ src/main/java/service/Owner.java
@@ -0,0 +1,44 @@
+package service;
+
+
+public class Owner {
+    
+    private int owner_id;
+    private String name;
+    private String phone_nomber;
+
+    public Owner() {
+    }
+
+    public Owner(int owner_id, String name, String phone_nomber) {
+        this.owner_id = owner_id;
+        this.name = name;
+        this.phone_nomber = phone_nomber;
+    }
+
+    public int getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(int owner_id) {
+        this.owner_id = owner_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone_nomber() {
+        return phone_nomber;
+    }
+
+    public void setPhone_nomber(String phone_nomber) {
+        this.phone_nomber = phone_nomber;
+    }
+    
+            
+}
---
+++ src/main/java/service/Pet.java
@@ -0,0 +1,83 @@
+package service;
+
+public class Pet {
+
+    private int client_number;
+    private String dog_name;
+    private String breed;
+    private String color;
+    private String allergic;
+    private String special_attention;
+
+    private Owner owner;
+
+    public Pet() {
+    }
+
+    public Pet(int client_number, String dog_name, String breed, String color, String allergic, String special_attention, Owner randomOwner) {
+        this.client_number = client_number;
+        this.dog_name = dog_name;
+        this.breed = breed;
+        this.color = color;
+        this.allergic = allergic;
+        this.special_attention = special_attention;
+        this.owner = randomOwner;
+    }
+
+    public int getClient_number() {
+        return client_number;
+    }
+
+    public void setClient_number(int client_number) {
+        this.client_number = client_number;
+    }
+
+    public String getDog_name() {
+        return dog_name;
+    }
+
+    public void setDog_name(String dog_name) {
+        this.dog_name = dog_name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAllergic() {
+        return allergic;
+    }
+
+    public void setAllergic(String allergic) {
+        this.allergic = allergic;
+    }
+
+    public String getSpecial_attention() {
+        return special_attention;
+    }
+
+    public void setSpecial_attention(String special_attention) {
+        this.special_attention = special_attention;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+}
