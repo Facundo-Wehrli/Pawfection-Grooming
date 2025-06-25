775aa53440dc001ec8d64c644719d0c0f02c015a
Facundo Gabriel Wehrli <wehrlifacundo9@gmail.com>
2025-06-25 08:09:53 -0300

    fix: change javax depencendy to jakarta

--- src/main/java/service/Pet.java
+++ src/main/java/service/Pet.java
@@ -3,9 +3,10 @@ package service;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import java.io.Serializable;
-import javax.persistence.Id;
+
 
 @Entity
 public class Pet implements Serializable {
